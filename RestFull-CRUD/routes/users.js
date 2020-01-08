b1e6f57ac0b567e01bbb64c753dcc0db48dde1a7
belliamatteo <bellia138@gmail.com>
2020-01-08 10:43:37 +0000

    fase bella

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -1,24 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const sql = require('mssql')
-
 const config = {
   user: '4DD_05',  //Vostro user name
   password: 'xxx123##', //Vostra password
-  server: "213.140.22.237\\sqlexpress",  //Stringa di connessione
-  //Oppure
-  //server: "213.140.22.237", //nel caso la stringa sopra non funzionasse
-  database: 'wtSchool', //(Nome del DB)
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_05', //(Nome del DB)
 }
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/BdT2_t2', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);  // ... error check
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from school.Person', (err, result) => {
+    sqlRequest.query('select * from dbo.AA_ATTORE', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
     });
